e8395afe93b31de8f76610a1d612d27fb0af7ece
Christy Crites <cacrites2007@gmail.com>
2018-03-28 18:01:53 -0400

    user-auth

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -3,40 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
 
-import Navbar from './navbar/navbar';
+import Navbar from './components/navbar';
 //import Home from './home'; change this to our file configuration
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: null,
-      fetching: true
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			message: null,
+			fetching: true
+		};
+	}
 
-  componentDidMount() {
-    fetch('/api')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        this.setState({
-          message: json.message,
-          fetching: false
-        });
-      }).catch(e => {
-        this.setState({
-          message: `API call failed: ${e}`,
-          fetching: false
-        });
-      })
-  }
+	componentDidMount() {
+		fetch('/api')
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`status ${response.status}`);
+				}
+				return response.json();
+			})
+			.then(json => {
+				this.setState({
+					message: json.message,
+					fetching: false
+				});
+			})
+			.catch(e => {
+				this.setState({
+					message: `API call failed: ${e}`,
+					fetching: false
+				});
+			});
+	}
 
-  render() {
+	render() {
 		return (
 			<div className="App">
 				<Navbar />
--- server/admin/adminEndpoints.js
+++ server/admin/adminEndpoints.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const admins = require('./adminController');
+const admins = require('./adminControllers');
 
 const adminRouter = express.Router();
 
