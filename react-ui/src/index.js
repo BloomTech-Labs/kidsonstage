102fd176acd255b21336d83b8ddd08a6c759697c
Christy Crites <cacrites2007@gmail.com>
2018-04-04 15:02:16 -0400

    signup page fixed

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -6,15 +6,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import App from './App';
-import SignIn from './components/SignIn';
-import Users from './components/users';
-import SignOut from './components/signout';
-import SignUp from './components/NewUser';
-import Settings from './components/Settings';
-import RequireAuth from './components/HOC/RequireAuth';
 
 import reducers from './reducers';
-import './index.css';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
 
@@ -23,12 +16,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router>
       <div>
-        <Route path="/" component={App} />
-        <Route path="/signin" component={SignIn} />
-        <Route path="/users" component={RequireAuth(Users)} />
-        <Route path="/signout" component={SignOut} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/settings" component={RequireAuth(Settings)} />
+        <App />
       </div>
     </Router>
   </Provider>,
