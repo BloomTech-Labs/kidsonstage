fa1237b76d85165521591752120f6aad2bee78cf
Tyler Sanford <tylersanford0311@gmail.com>
2018-04-11 18:40:35 -0500

    Events displaying

--- react-ui/src/components/Events.js
+++ react-ui/src/components/Events.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import { getEvents } from '../actions';
 
