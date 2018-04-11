4c691812c7eb18ba59623872e1959be3567eb133
Tyler Sanford <tylersanford0311@gmail.com>
2018-04-11 14:17:49 -0500

    Sendgrid and Twilio integration working - disabled for now. Updated users in the seeds.

---
+++ server/endpoints/dispatch/sendGridDispatch.js
@@ -0,0 +1,23 @@
+require('dotenv').config({path: '../../.env'});
+
+const sgMail = require('@sendgrid/mail');
+const moment = require('moment');
+
+export function sendGridDispatch(emailArr, name, difference) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  
+  console.log(difference);
+
+  let message = {
+    to: emailArr,
+    from: 'admin@kidsonstage.herokuapp.com',
+    templateId: 'b3f63a64-a93a-4678-bfdc-4185f796989f',
+    substitutions: {
+      group: name,
+      remaining: difference
+    }
+  };
+
+  // DISABLED UNTIL LIVE
+  // sgMail.sendMultiple(message);
+}
