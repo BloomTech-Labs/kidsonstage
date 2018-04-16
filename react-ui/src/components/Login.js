9e6e2537dc6d1679c9cb05a7888de74ee98d2cc3
Tyler Sanford <tylersanford0311@gmail.com>
2018-04-16 10:20:31 -0500

    Added styling to signin and signup

--- react-ui/src/components/Login.js
+++ react-ui/src/components/Login.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { login } from '../actions';
 
+import './css/login.css';
+
 class SignIn extends Component {
   handleFormSubmit = ({ username, password }) => {
     // console.log(`username: ${username} password: ${password}`);
@@ -20,34 +22,41 @@ class SignIn extends Component {
     const { handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.handleFormSubmit)}>
-        <fieldset>
-          <label>Username:</label>
-          <Field name="username" component="input" type="text" />
-        </fieldset>
-        <fieldset>
-          <label>Password:</label>
-          <Field name="password" component="input" type="password" />
-        </fieldset>
-        <button action="submit">Sign In</button>
-        {this.renderAlert()}
-      </form>
+      <div className="login--container">
+        <div className="login--form_container">
+          <div className="login--icon"></div>
+          <div className="login--form">
+            <form onSubmit={handleSubmit(this.handleFormSubmit)}>
+              <fieldset>
+                
+                <Field className="login--form_field" name="username" component="input" type="text" placeholder="Username"/>
+              </fieldset>
+              <fieldset>
+                
+                <Field className="login--form_field" name="password" component="input" type="password" placeholder="Password" />
+              </fieldset>
+              <button action="submit">Sign In</button>
+              {this.renderAlert()}
+            </form>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
 SignIn.defaultProps = {
-  error: null,
+  error: null
 };
 SignIn.propTypes = {
   login: PropTypes.func.isRequired,
   error: PropTypes.string,
   handleSubmit: PropTypes.func.isRequired,
-  history: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
 };
 const mapStateToProps = state => ({
   error: state.auth.error,
-  authenticated: state.auth.authenticated,
+  authenticated: state.auth.authenticated
 });
 
 SignIn = connect(mapStateToProps, { login })(SignIn);
@@ -56,7 +65,7 @@ SignIn = connect(mapStateToProps, { login })(SignIn);
 
 export default reduxForm({
   form: 'signin',
-  fields: ['username', 'password'],
+  fields: ['username', 'password']
 })(SignIn);
 
 // export default reduxForm({
--- react-ui/src/components/Register.js
+++ react-ui/src/components/Register.js
@@ -4,18 +4,31 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import normalizePhone from './normalizers/normalizePhone';
 import { register } from '../actions';
-import './css/Register.css';
+import './css/register.css';
 
 // const { DOM: { input /* select, textarea */ } } = React;
 
 class Register extends Component {
-  handleFormSubmit = ({ username = 'mark', email = 'mark@gmail', phoneNumber = '9999999999',
-    password = 'p', confirmPassword = 'p', byPhone, byEmail }) => {
+  handleFormSubmit = ({
+    username = 'mark',
+    email = 'mark@gmail',
+    phoneNumber = '9999999999',
+    password = 'p',
+    confirmPassword = 'p',
+    byPhone,
+    byEmail
+  }) => {
     this.props.register(
       {
-        username, email, phoneNumber, password, confirmPassword, byPhone, byEmail,
+        username,
+        email,
+        phoneNumber,
+        password,
+        confirmPassword,
+        byPhone,
+        byEmail
       },
-      this.props.history,
+      this.props.history
     );
   };
 
@@ -42,99 +55,131 @@ class Register extends Component {
     //   </div>
     // );
     const email = value =>
-      (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+      value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
         ? 'Invalid email address'
-        : undefined);
+        : undefined;
     const aol = value =>
-      (value && /.+@aol\.com/.test(value)
+      value && /.+@aol\.com/.test(value)
         ? 'Really? You still use AOL for your email?'
-        : undefined);
+        : undefined;
     return (
-      <div className = "register-box">
-      <form onSubmit={handleSubmit(this.handleFormSubmit)} id="submit-button" >
-        <div className="register-form">
-          <label className="label">Username:</label>
-          <Field
-            name="username"
-            component="input"
-            type="text"
-            className="inputText"
-            placeholder="User Name"
-            //id="username"
-          />
-        </div>
-        <div className="register-form">
-          <label className="label">Email</label>
-          <Field
-            name="email"
-            component="input"
-            validate={email}
-            warn={aol}
-            placeholder="Email"
-            className="inputText"
-            //id="email"
-          />
-        </div>
-        <div className="register-form">
-          <label className="label">Phone Number</label>
-          <Field
-            name="phone"
-            className="inputText"
-            component="input"
-            type="text"
-            placeholder="Phone Number"
-            normalize={normalizePhone}
-            //id="phone"
-          />
-        </div>
-        <div className="register-form">
-          <label className="label">Password:</label>
-          <Field
-            name="password"
-            component="input"
-            type="password"
-            className="inputText"
-            placeholder="Password"
-          />
-        </div>
-        <div className="register-form">
-          <label className="label">Confirm Password:</label>
-          <Field
-            name="confirmPassword"
-            component="input"
-            type="password"
-            className="inputText"
-            placeholder="Confirm Password"
-          />
-        </div>
-        <div className="contact-text" >How would you like to be contacted?</div>
-        <div className="register-form" id="checkbox">
-          <div id="text-preference">
-            <Field name="byPhone" id="byPhone" component="input" type="checkbox" className="checkbox" />
-            <span>Texts?</span>
+      <div className="register--container">
+        <div className="register--form_container">
+          <div className="register--icon" />
+          <div className="register--form">
+            <form
+              onSubmit={handleSubmit(this.handleFormSubmit)}
+              id="submit-button"
+            >
+              <fieldset>
+                <Field
+                  className="register--form_field"
+                  name="username"
+                  component="input"
+                  type="text"
+                  placeholder="User Name"
+                  //id="username"
+                />
+              </fieldset>
+              <fieldset>
+                <Field
+                  className="register--form_field"
+                  name="email"
+                  component="input"
+                  validate={email}
+                  warn={aol}
+                  placeholder="Email"
+                  //id="email"
+                />
+              </fieldset>
+              <fieldset>
+                <Field
+                  className="register--form_field"
+                  name="phone"
+                  component="input"
+                  type="text"
+                  placeholder="Phone Number"
+                  normalize={normalizePhone}
+                  //id="phone"
+                />
+              </fieldset>
+              <fieldset>
+                <Field
+                  className="register--form_field"
+                  name="password"
+                  component="input"
+                  type="password"
+                  placeholder="Password"
+                />
+              </fieldset>
+              <fieldset>
+                <Field
+                  className="register--form_field"
+                  name="confirmPassword"
+                  component="input"
+                  type="password"
+                  placeholder="Confirm Password"
+                />
+              </fieldset>
+
+              <div className="contact-text">
+                How would you like to be contacted?
+              </div>
+
+              <fieldset>
+                <Field
+                  className="register--form_field"
+                  name="byPhone"
+                  id="byPhone"
+                  component="input"
+                  type="checkbox"
+                  className="checkbox"
+                />
+                <span>Texts? </span>
+                <Field
+                  className="register--form_field"
+                  name="byEmail"
+                  id="byEmail"
+                  component="input"
+                  type="checkbox"
+                  className="checkbox"
+                />
+                <span>Email?</span>
+              </fieldset>
+              <div className="register--form_buttons">
+                <button type="submit" disabled={pristine || submitting}>
+                  Sign Up
+                </button>
+                <button
+                  type="button"
+                  disabled={pristine || submitting}
+                  onClick={reset}
+                >
+                  Reset
+                </button>
+              </div>
+            </form>
           </div>
-          <div id="email-preference">
-            <Field name="byEmail" id="byEmail" component="input" type="checkbox" className="checkbox" />
-            <span>Email?</span>
-          </div>
         </div>
-        <div className="register-form">
-          <button className="button" type="submit" disabled={pristine || submitting}>Sign Up</button>
-          <button className="button" type="button" disabled={pristine || submitting} onClick={reset}>Reset</button>
-        </div>
-      </form>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  error: state.auth.error,
+  error: state.auth.error
 });
 
 export default reduxForm({
   form: 'newUser',
   touchOnBlur: true,
-  fields: ['username', 'email', 'phone', 'password',
-    'confirmPassword', 'byPhone', 'byEmail'],
+  fields: [
+    'username',
+    'email',
+    'phone',
+    'password',
+    'confirmPassword',
+    'byPhone',
+    'byEmail'
+  ]
 })(connect(mapStateToProps, { register })(Register));
