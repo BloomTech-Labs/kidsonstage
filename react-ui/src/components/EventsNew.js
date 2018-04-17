b7143fdd26b2da5d95ed9d0b241fef05edc040ac
Tyler Sanford <tylersanford0311@gmail.com>
2018-04-16 23:45:58 -0500

    EventsNew CSS

--- react-ui/src/components/EventsNew.js
+++ react-ui/src/components/EventsNew.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
+import { Progress } from 'reactstrap';
+
 import './css/events.css';
 
 class EventsNew extends Component {
   render() {
     return (
       <div>
-        Create New Event
+        Add New Event
+        <Progress value="50">Step 1 of 2 - Create Event</Progress>
       </div>
     );
   }
