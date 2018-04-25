01a2743f2131bf545c4bd753b7481ca396c3edcc
Mark Oliver <mark.c.oliver@gmail.com>
2018-04-24 21:22:56 -0400

    persist non-owner events, restore line-through on complete

--- react-ui/src/reducers/invitedEvents.js
+++ react-ui/src/reducers/invitedEvents.js
@@ -1,11 +1,23 @@
 import { GET_INVITED_EVENTS, ADD_INVITED_EVENT, DELETE_INVITED_EVENT } from '../actions';
 
+const lastIndex = (arr, item) => {
+  let i = arr.length - 1;
+  for (; i >= 0; i--) {
+    if (item.eventId === arr[i].eventId) {
+      break;
+    }
+  }
+  return i;
+};
+
 export default (invitedEvents = [], action) => {
   switch (action.type) {
     case GET_INVITED_EVENTS:
+      console.log(`reducer action.payload.length: ${action.payload.length}`);
       return action.payload;
     case ADD_INVITED_EVENT:
-      return [action.payload, ...invitedEvents];
+      return [action.payload, ...invitedEvents]
+        .filter((item, i, self) => lastIndex(self, item) === i); // remove dups
     case DELETE_INVITED_EVENT:
       return invitedEvents.filter(event => event.id !== action.payload.id);
     // case ADD_EVENT_GR
